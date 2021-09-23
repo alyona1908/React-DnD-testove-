5b50b71749c18545135a3244ca6a698214bb061d
Alyona1908 <Alyona.narozhna@gmail.com>
2021-09-23 10:24:07 +0300

    srch

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -15,8 +15,10 @@ function ModalWind() {
     const {columns} = useSelector(store => store.table);
     const dispatch = useDispatch()
     const [lgShow, setLgShow] = useState(false);
-    const [controlledBoard, setBoard] = useState(columns);
     const [value, setValue] = useState('');
+
+    const [controlledBoard, setBoard] = useState(columns);
+  
 
     useEffect(() => {
         dispatch(getTable())
@@ -29,34 +31,71 @@ function ModalWind() {
     },[columns])
 
 
-    let filteredColumns;
-    if (Object.keys(columns).length !== 0) {
-      filteredColumns = columns.columns[0].cards.filter((el) => {
-        el.title.toLowerCase().includes(value.toLowerCase())
-      })
+
+    const filteredColumns = () => {
+      const cols = columns?.columns?.[0]?.cards;
+      
+      return cols && cols.length !== 0 ? columns.columns[0].cards.filter((el) => {
+        return el.title.toLowerCase().includes(value.toLowerCase())
+      }) : [];
     }
+    console.log(filteredColumns(),'1111');
 
     const ControlledBoard = () => {
   
         function handleCardMove(_card, source, destination) {
           const updatedBoard = moveCard(controlledBoard, source, destination);
           setBoard(updatedBoard);
-        }
 
-        console.log("controlledBoard",controlledBoard)
+          if (updatedBoard.columns[1].cards.length !== columns.columns[1].cards.length){
+              if (updatedBoard.columns[1].cards.length > columns.columns[1].cards.length){
+                for (let i = 0;i<updatedBoard.columns[1].cards.length;i++){
+                  let is_duble = false;
+                  for (let j = 0;j<columns.columns[1].cards.length;j++){
+                  if (columns.columns[1].cards[j] == updatedBoard.columns[1].cards[i]){
+                    is_duble = true;
+                    break;
+                  }
+                }
+                if (! is_duble){
+                  columns.columns[0].cards.splice(columns.columns[0].cards.indexOf(updatedBoard.columns[1].cards[i]), 1);
+                  columns.columns[1].cards.splice(i,0,updatedBoard.columns[1].cards[i]);
+                }
+              }
+              }else{
+                for (let i = 0;i<columns.columns[1].cards.length;i++){
+                  let is_duble = false;
+                  for (let j = 0;j<updatedBoard.columns[1].cards.length;j++){
+                  if (columns.columns[1].cards[i] == updatedBoard.columns[1].cards[j]){
+                    is_duble = true;
+                    break;
+                  }
+                }
+                if (! is_duble){
+                  columns.columns[0].cards.splice(updatedBoard.columns[0].cards.indexOf(columns.columns[1].cards[i]),0,columns.columns[1].cards[i]);
+                  columns.columns[1].cards.splice(columns.columns[1].cards.indexOf(columns.columns[1].cards[i]), 1);
+                }
+              }
+              }
+          }
+          
+        }
     
+        let clmn = JSON.parse(JSON.stringify(controlledBoard));
+        clmn.columns[0].cards =  filteredColumns();
         return (
           <Board onCardDragEnd={handleCardMove} disableColumnDrag>
-            {controlledBoard}
+            {clmn}
           </Board>
         );
     }
 
     const onClickHandler = () => {
         console.log("eyaifa")
-        dispatch({type: GET_TABLE, payload: controlledBoard})
         setLgShow(false)
-    }
+        setValue('');
+        dispatch({type: GET_TABLE, payload: controlledBoard})
+      }
 
  
 
